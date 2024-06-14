52c95a0175dd6d59434f28d4818feaf47c92eb25
ElisabettaMirti <belmirti@gmail.com>
2024-06-14 12:51:33 +0200

    provato a fare l'infrastruttura

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,16 +4,41 @@ import {store} from '../store.js';
 
 export default {
     data(){
-    return{
-        store
-    }
+        return{
+            store,
+            requestedMovie: ''
+        }
+    },
+    methods: {
+        getMovie(movieTitle){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=77858b6fb6570fc530c9dcb381e6d68f&language=it&query=' + requestedMovie.replaceAll('', '+'))
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                store.SearchedMovie = response;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        }
     }
 }
 </script>
 
 <template>
 <label for="name">Search a movie:</label>
-<input type="text" id="name" />
+<input type="text" id="name" v-model="this.requestedMovie"/>
+<input type="button" value="Cerca" @click="getMovie(this.requestedMovie)"/>
+
+<ul>
+    <li v-for="(movie, index) in store.SearchedMovie" :key="index">
+        {{ store.SearchedMovie.title }}
+    </li>
+</ul>
 </template>
 
 <style lang="scss">
