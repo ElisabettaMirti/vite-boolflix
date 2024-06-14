62c5855a4e001ab0eb147fa9cf2f042cb67ba940
ElisabettaMirti <belmirti@gmail.com>
2024-06-14 12:11:03 +0200

    aggiunto store.js

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,22 @@
 <script>
 import axios from 'axios';
+import {store} from '../store.js';
 
 export default {
     data(){
-    return{}
+    return{
+        store
+    }
     }
 }
 </script>
 
 <template>
-
+<label for="name">Search a movie:</label>
+<input type="text" id="name" />
 </template>
 
 <style lang="scss">
 
-@use './styles/general.scss';
+@use '../styles/general.scss';
 </style>
